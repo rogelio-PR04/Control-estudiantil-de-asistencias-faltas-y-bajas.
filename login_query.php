eb965942fa284b8fdaf052a8a2ebdac7f0e3d3b8
rogelio-PR04 <89054718+rogelio-PR04@users.noreply.github.com>
2024-11-19 17:24:51 -0600

    Update login_query.php

--- login_query.php
+++ login_query.php
@@ -1,31 +1,31 @@
 <?php
-        // Conectar a la base de datos
-	$db = new SQLite3('testDB');
-       
-        if(ISSET($_POST['register'])){
-                //Estableciendo las variables
-                $Correo = $_POST['Correo'];
-                $Contrasenia = $_POST['Contrasenia'];
-               
-                // Consulta de inserción (Insert Query)
-		try {
-			// Realizar una consulta para verificar la conexión
-			$result = $db->query('INSERT INTO `Data` (Correo, Contrasenia) VALUES(:Correo, :Contrasenia)');
-                	$stmt->bindParam(':Correo', $Correo);
-                	$stmt->bindParam(':Contrasenia', $Contrasenia);
-                
-			// Comprobar si la ejecución es un éxito
-			if($stmt->execute()){
-                        	//Fijando un éxito en la sesión ('success' session) para guardar el mensaje de éxito.
-                        	$_SESSION['success'] = "Usuario guardado con éxito";
- 
-                        	//Redirección al módulo de 'Alumnos'
-                        	header('location: Alumnos.php');
-                	}
-		
-		} catch (Exception $e) {
-			echo "Error en la consulta: " . $e->getMessage();
-		}
- 
-        }
+// Conectar a la base de datos SQLite
+$database = new SQLite3('Crtl_Stdtl.db');
+
+if(ISSET($_POST['register'])){
+
+// Datos de ejemplo
+$Correo = $_POST['Correo'];
+$Contrasenia = $_POST['Contrasenia'];
+
+try {
+
+// Escapar las entradas
+$Correo = $database->escapeString($Correo);
+$Contrasenia = $database->escapeString($Contrasenia);
+
+// Insertar los datos en la tabla usuarios
+$query = "INSERT INTO Data (Correo, Contrasenia) VALUES ('$Correo', '$Contrasenia')";
+$database->exec($query);
+
+header('location: Alumnos.php');
+
+} catch (Exception $e) {
+echo "Error en la consulta: " . $e->getMessage();
+}
+
+}
+
+// Cerrar la conexión a la base de datos
+$database->close();
 ?>
